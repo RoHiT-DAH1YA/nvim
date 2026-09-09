597bc89e611f663407ddb91b4cdc0a0faab0a40c
half_Owl <rohitdahiya102852@gmail.com>
2026-09-09 13:44:29 +0530

    minor changes in autoindent options. Made them explicitly autoindent

--- lua/options.lua
+++ lua/options.lua
@@ -16,6 +16,8 @@ vim.opt.listchars = {
 }
 
 -- Set default indentation to four spaces
+vim.opt.autoindent = true
+vim.opt.smartindent = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
