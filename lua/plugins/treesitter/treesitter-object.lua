9c155e3480e09a339b420b68b36ed266faeaa212
half_Owl <rohitdahiya102852@gmail.com>
2026-09-09 14:16:27 +0530

    Updated treesitter to a new branch. There was a complete rewrite and needed explicit update

--- lua/plugins/treesitter/treesitter-object.lua
+++ lua/plugins/treesitter/treesitter-object.lua
@@ -1,4 +1,42 @@
 return {
     "nvim-treesitter/nvim-treesitter-textobjects",
-}
+    branch = "main",
 
+    config = function()
+        local textobject = require("nvim-treesitter-textobjects.select")
+
+        require("nvim-treesitter-textobjects").setup({
+            select = {
+                lookahead = true,
+
+                selection_modes = {
+                    ["@parameter.outer"] = "v",
+                    ["@function.outer"] = "V",
+                    ["@class.outer"] = "<c-v>",
+                },
+
+                include_surrounding_whitespace = true,
+            },
+        })
+
+        vim.keymap.set({ "x", "o" }, "af", function()
+            textobject.select_textobject("@function.outer", "textobjects")
+        end)
+
+        vim.keymap.set({ "x", "o" }, "if", function()
+            textobject.select_textobject("@function.inner", "textobjects")
+        end)
+
+        vim.keymap.set({ "x", "o" }, "ac", function()
+            textobject.select_textobject("@class.outer", "textobjects")
+        end)
+
+        vim.keymap.set({ "x", "o" }, "ic", function()
+            textobject.select_textobject("@class.inner", "textobjects")
+        end)
+
+        vim.keymap.set({ "x", "o" }, "as", function()
+            textobject.select_textobject("@scope", "locals")
+        end)
+    end,
+}
--- lua/plugins/treesitter/treesitter.lua
+++ lua/plugins/treesitter/treesitter.lua
@@ -1,48 +1,39 @@
 return {
-
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
-    config = function () 
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript","html" },
 
-            auto_install = true,
-            highlight = {
-                enable = true, -- enable highlighting by default
-            },
-            indent = {
-                enable = true;
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<Leader>ss", -- set to `false` to disable one of the mappings
-                    node_incremental = "<Leader>si",
-                    scope_incremental = "<Leader>so",
-                    node_decremental = "<Leader>sn",
-                },
-            },
-            textobjects = {
-                select = {
-                    enable = true,
-                    -- Automatically jump forward to textobj, similar to targets.vim
-                    lookahead = true,
-                    keymaps = {
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["ac"] = "@class.outer",
-                        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-                        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-                    },
-                    selection_modes = {
-                        ['@parameter.outer'] = 'v', -- charwise
-                        ['@function.outer'] = 'V', -- linewise
-                        ['@class.outer'] = '<c-v>', -- blockwise
-                    },
-                    include_surrounding_whitespace = true,
-                },
-            },
+    config = function()
+        local ts = require("nvim-treesitter")
+
+        ts.install({
+            "c",
+            "cpp",
+            "lua",
+            "vim",
+            "vimdoc",
+            "query",
+            "javascript",
+            "html",
         })
-    end
-}
 
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = {
+                "c",
+                "cpp",
+                "lua",
+                "vim",
+                "vimdoc",
+                "query",
+                "javascript",
+                "html",
+            },
+            callback = function()
+                vim.treesitter.start()
+                -- set the appropriate indentexpr
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
+    end,
+}
